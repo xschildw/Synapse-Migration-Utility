4bf15ba87417680c5a2e92ce361930a04681d8d1
john-hill <john.hill@sagebase.org>
2019-02-14 17:58:46 -0800

    phase one is completed before starting phase two

--- src/test/java/org/sagebionetworks/migration/async/MigrationDriverImplTest.java
+++ src/test/java/org/sagebionetworks/migration/async/MigrationDriverImplTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.sagebionetworks.migration.async.checksum.ChecksumDeltaBuilder;
 import org.sagebionetworks.migration.config.Configuration;
 import org.sagebionetworks.migration.utils.TypeToMigrateMetadata;
 import org.sagebionetworks.repo.model.migration.MigrationType;
@@ -26,7 +27,9 @@ public class MigrationDriverImplTest {
 	@Mock
 	Configuration mockConfig;
 	@Mock
-	DestinationJobBuilder mockJobBuilder;
+	MissingFromDestinationBuilder mockMissingFromDestinationBuilder;
+	@Mock
+	ChecksumDeltaBuilder mockChecksumChangeBuilder;
 	@Mock
 	Clock mockClock;
 	@Mock
@@ -36,7 +39,8 @@ public class MigrationDriverImplTest {
 	RestoreDestinationJob jobOne;
 	RestoreDestinationJob jobTwo;
 	RestoreDestinationJob jobThree;
-	List<DestinationJob> destinationJobs;
+	List<DestinationJob> missingJobs;
+	List<DestinationJob> deltaJobs;
 
 	MigrationDriverImpl migrationDriver;
 
@@ -45,8 +49,9 @@ public class MigrationDriverImplTest {
 		jobOne = new RestoreDestinationJob(MigrationType.NODE, "someKey1");
 		jobTwo = new RestoreDestinationJob(MigrationType.NODE, "someKey2");
 		jobThree = new RestoreDestinationJob(MigrationType.NODE, "someKey3");
-		destinationJobs = Lists.newArrayList(jobOne,jobTwo, jobThree);
-		
+		missingJobs = Lists.newArrayList(jobOne, jobTwo);
+		deltaJobs = Lists.newArrayList(jobThree);
+
 		TypeToMigrateMetadata toMigrate = new TypeToMigrateMetadata();
 		toMigrate.setType(MigrationType.NODE);
 		toMigrate.setSrcMinId(1L);
@@ -56,35 +61,38 @@ public class MigrationDriverImplTest {
 		toMigrate.setDestMaxId(4L);
 		toMigrate.setDestCount(3L);
 		primaryTypes = Lists.newArrayList(toMigrate);
-		
-		when(mockJobBuilder.buildDestinationJobs(primaryTypes)).thenReturn(destinationJobs.iterator());
-		when(mockRestoreJobQueue.isDone()).thenReturn(false,false,true);
-		
-		migrationDriver = new MigrationDriverImpl(mockConfig, mockJobBuilder, mockRestoreJobQueue, mockClock);
+
+		when(mockMissingFromDestinationBuilder.buildDestinationJobs(primaryTypes)).thenReturn(missingJobs.iterator());
+		when(mockChecksumChangeBuilder.buildAllRestoreJobsForMismatchedChecksums(primaryTypes))
+				.thenReturn(deltaJobs.iterator());
+		when(mockRestoreJobQueue.isDone()).thenReturn(false, false, true);
+
+		migrationDriver = new MigrationDriverImpl(mockConfig, mockMissingFromDestinationBuilder,
+				mockChecksumChangeBuilder, mockRestoreJobQueue, mockClock);
 	}
-	
-	
+
 	@Test
 	public void testMigratePrimaryTypes() throws InterruptedException {
 		// call under test
 		migrationDriver.migratePrimaryTypes(primaryTypes);
-		verify(mockJobBuilder).buildDestinationJobs(primaryTypes);
+		verify(mockMissingFromDestinationBuilder).buildDestinationJobs(primaryTypes);
+		verify(mockChecksumChangeBuilder).buildAllRestoreJobsForMismatchedChecksums(primaryTypes);
 		// Three jobs should be pushed to the queue
 		verify(mockRestoreJobQueue, times(3)).pushJob(any(DestinationJob.class));
 		verify(mockRestoreJobQueue).pushJob(jobOne);
 		verify(mockRestoreJobQueue).pushJob(jobOne);
 		verify(mockRestoreJobQueue).pushJob(jobOne);
-		
+
 		// Should sleep twice waiting for the restore jobs to finish.
 		verify(mockClock, times(2)).sleep(MigrationDriverImpl.SLEEP_TIME_MS);
 	}
-	
-	@Test (expected=RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void testMigratePrimaryTypesInterupt() throws InterruptedException {
 		// Interrupt should become runtime.
 		doThrow(new InterruptedException()).when(mockClock).sleep(any(Long.class));
 		// call under test
 		migrationDriver.migratePrimaryTypes(primaryTypes);
 	}
-	
+
 }
